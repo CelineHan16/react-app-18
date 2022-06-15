139ca6561bb596477a96fdc0db0ae9fc48be9212
syrgo <syrga.v30@gmail.com>
2022-06-15 23:14:13 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Routes, Route } from "react-router-dom";
+import ArticleCreate from "./components/ArticleCreate";
+import ArticleDelete from "./components/ArticleDelete";
+import ArticleFull from "./components/ArticleFull";
+import ArticleList from "./components/ArticleList";
+import ArticleUpdate from "./components/ArticleUpdate";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<ArticleList />} />
+        <Route path="/:id" element={<ArticleFull />} />
+        <Route path="/create" element={<ArticleCreate />} />
+        <Route path="/update/:id" element={<ArticleUpdate />} />
+        <Route path="/delete/:id" element={<ArticleDelete />} />
+      </Routes>
     </div>
   );
 }
